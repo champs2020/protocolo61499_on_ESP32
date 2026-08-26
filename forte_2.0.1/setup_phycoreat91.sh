478ee26ff5346cbd7f21845ee2692498688f163d
Ze Hélio Jr <jose.helio.junior@ee.ufcg.edu.br>
2026-08-26 17:26:22 -0300

    chore: replace forte_2.0.1 with forte_2 runtime source

--- forte_2.0.1/setup_phycoreat91.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "----------------------------------------------------------------------------"
-echo "BUILD-SYSTEM was switched from automake to cmake"
-echo "----------------------------------------------------------------------------"
-echo ""
-echo "run cmake or cmake-gui to set up development environment"
